5d2af47056d08f833817b4bbc7b858c1359ff5d1
alex_mclean <116386669+EatSleepProgramRepeat@users.noreply.github.com>
2025-04-06 16:29:15 -0400

    fixed JUnit ver mismatch

--- src/main/java/com/CDPrintable/Constants.java
+++ src/main/java/com/CDPrintable/Constants.java
@@ -2,7 +2,7 @@ package com.CDPrintable;
 
 public class Constants {
     // MAJOR MINOR PATCH
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.2.2";
 
     public static final boolean USER_AGENT_EMAIL_CHANGED = false;
 }
