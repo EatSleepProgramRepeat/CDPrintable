06f8c92e435b2cf850eb448fb7658c3d59d8c229
alex_mclean <116386669+EatSleepProgramRepeat@users.noreply.github.com>
2025-04-07 07:30:43 -0400

    a bunch of legal jonqlers

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
+/*
+ * CDPrintable: A program that prints labels with track listings for your CD cases.
+ * Copyright (C) 2025 Alexander McLean
+ *
+ * This source code is licensed under the GNU General Public License v3.0
+ * found in the LICENSE file in the root directory of this source tree.
+ *
+ * This is a settings file for the Gradle build system.
+ */
+
 rootProject.name = "CDPrintable"
 
--- src/main/java/com/CDPrintable/MusicBrainzRequest.java
+++ src/main/java/com/CDPrintable/MusicBrainzRequest.java
@@ -1,3 +1,13 @@
+/*
+ * CDPrintable: A program that prints labels with track listings for your CD cases.
+ * Copyright (C) 2025 Alexander McLean
+ *
+ * This source code is licensed under the GNU General Public License v3.0
+ * found in the LICENSE file in the root directory of this source tree.
+ *
+ * This class helps you build a URL request to the MusicBrainz API.
+ */
+
 package com.CDPrintable;
 
 /**
