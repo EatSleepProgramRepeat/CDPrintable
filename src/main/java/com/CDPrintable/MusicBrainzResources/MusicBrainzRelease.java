da410bd460be80ca5ebe069c5c04c0e5bd8d3c53
alex_mclean <116386669+EatSleepProgramRepeat@users.noreply.github.com>
2025-04-10 18:14:27 -0400

    made a release class bc i needed to

---
+++ src/main/java/com/CDPrintable/MusicBrainzResources/MusicBrainzRelease.java
@@ -0,0 +1,75 @@
+/*
+ * CDPrintable: A program that prints labels with track listings for your CD cases.
+ * Copyright (C) 2025 Alexander McLean
+ *
+ * This source code is licensed under the GNU General Public License v3.0
+ * found in the LICENSE file in the root directory of this source tree.
+ *
+ * This is a class that stores release data from the JSON reader.
+ */
+
+package com.CDPrintable.MusicBrainzResources;
+
+public class MusicBrainzRelease {
+    private String title;
+    private String[] artists;
+    private int year;
+    private int trackCount;
+    private String id;
+
+    public MusicBrainzRelease(String title, String[] artists, int year, int trackCount, String id) {
+        this.title = title;
+        this.artists = artists;
+        this.year = year;
+        this.trackCount = trackCount;
+        this.id = id;
+    }
+
+    public String getArtistsAsString() {
+        StringBuilder sb = new StringBuilder();
+        for (String artist : artists) {
+            sb.append(artist).append(", ");
+        }
+        return sb.substring(0, sb.length() - 2);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getTrackCount() {
+        return trackCount;
+    }
+
+    public void setTrackCount(int trackCount) {
+        this.trackCount = trackCount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String[] getArtists() {
+        return artists;
+    }
+
+    public void setArtists(String[] artists) {
+        this.artists = artists;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
