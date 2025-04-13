995bb443e5c06d9ced6582c494cd373474563bb4
alex_mclean <116386669+EatSleepProgramRepeat@users.noreply.github.com>
2025-04-13 14:52:55 -0400

    made Gender enum

---
+++ src/main/java/com/CDPrintable/MusicBrainzResources/Gender.java
@@ -0,0 +1,19 @@
+/*
+ * CDPrintable: A program that prints labels with track listings for your CD cases.
+ * Copyright (C) 2025 Alexander McLean
+ *
+ * This source code is licensed under the GNU General Public License v3.0
+ * found in the LICENSE file in the root directory of this source tree.
+ *
+ * This is an enum to represent genders.
+ */
+
+package com.CDPrintable.MusicBrainzResources;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    NON_BINARY,
+    OTHER,
+    UNKNOWN
+}
