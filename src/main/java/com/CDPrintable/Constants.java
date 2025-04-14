85fee4e14e2a9ac12cb0ff524f68138118091117
alex_mclean <116386669+EatSleepProgramRepeat@users.noreply.github.com>
2025-04-13 22:14:40 -0400

    set up multithreading

--- src/main/java/com/CDPrintable/Constants.java
+++ src/main/java/com/CDPrintable/Constants.java
@@ -15,4 +15,7 @@ public class Constants {
     public static final String VERSION = "1.6.4";
 
     public static final boolean USER_AGENT_EMAIL_CHANGED = false;
+
+    public static final int MAX_THREADS = 4;
+    public static final ThreadManager THREAD_MANAGER = new ThreadManager();
 }
