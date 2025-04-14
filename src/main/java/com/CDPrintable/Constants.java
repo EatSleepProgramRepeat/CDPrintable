80337fe75f14ae3e5362cc236d0c844013b62c2e
alex_mclean <116386669+EatSleepProgramRepeat@users.noreply.github.com>
2025-04-13 23:07:42 -0400

    fixed gradle errors and made version.properties work with gradle

--- src/main/java/com/CDPrintable/Constants.java
+++ src/main/java/com/CDPrintable/Constants.java
@@ -10,9 +10,27 @@
 
 package com.CDPrintable;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Constants {
     // MAJOR MINOR PATCH
-    public static final String VERSION = "1.6.4";
+    public static final String VERSION;
+    static {
+        Properties prop = new Properties();
+        String tempVersion = "0.0.0"; // Default version
+        try (InputStream input = Constants.class.getClassLoader().getResourceAsStream("version.properties")) {
+            if (input != null) {
+                prop.load(input);
+                tempVersion = prop.getProperty("version");
+            } else {
+                System.err.println("version.properties not found in resources.");
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to load version.properties: " + e.getMessage());
+        }
+        VERSION = tempVersion;
+    }
 
     public static final boolean USER_AGENT_EMAIL_CHANGED = false;
 
