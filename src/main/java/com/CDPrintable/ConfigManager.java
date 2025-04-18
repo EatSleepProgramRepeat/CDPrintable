67a7a83b1e56773daf3e42596c072fee8a4f8d04
Alexander McLean <mcleanalexander37@gmail.com>
2025-04-18 11:35:34 -0400

    made the class

---
+++ src/main/java/com/CDPrintable/ConfigManager.java
@@ -0,0 +1,14 @@
+/*
+ * CDPrintable: A program that prints labels with track listings for your CD cases.
+ * Copyright (C) 2025 Alexander McLean
+ *
+ * This source code is licensed under the GNU General Public License v3.0
+ * found in the LICENSE file in the root directory of this source tree.
+ *
+ * This class reads and writes to the config file for the program.
+ */
+
+package com.CDPrintable;
+
+public class ConfigManager {
+}
