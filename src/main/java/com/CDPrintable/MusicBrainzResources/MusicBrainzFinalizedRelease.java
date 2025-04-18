c7ac23c2adb67752ccdc1aa523145b67ce0b2ec7
Alexander McLean <mcleanalexander37@gmail.com>
2025-04-18 16:56:37 -0400

    made file

---
+++ src/main/java/com/CDPrintable/MusicBrainzResources/MusicBrainzFinalizedRelease.java
@@ -0,0 +1,14 @@
+/*
+ * CDPrintable: A program that prints labels with track listings for your CD cases.
+ * Copyright (C) 2025 Alexander McLean
+ *
+ * This source code is licensed under the GNU General Public License v3.0
+ * found in the LICENSE file in the root directory of this source tree.
+ *
+ * This class represents the final album data that will be used to print the CD label.
+ */
+
+package com.CDPrintable.MusicBrainzResources;
+
+public class MusicBrainzFinalizedRelease {
+}
