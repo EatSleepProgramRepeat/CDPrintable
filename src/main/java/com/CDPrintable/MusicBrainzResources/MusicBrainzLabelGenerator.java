a0c19218831778d3cb2d4a7ac4652b812a8ac0a9
Alexander McLean <mcleanalexander37@gmail.com>
2025-04-19 09:40:29 -0400

    got things printing :D

--- src/main/java/com/CDPrintable/MusicBrainzResources/MusicBrainzLabelGenerator.java
+++ src/main/java/com/CDPrintable/MusicBrainzResources/MusicBrainzLabelGenerator.java
@@ -10,10 +10,27 @@
 
 package com.CDPrintable.MusicBrainzResources;
 
+import java.awt.*;
+import java.awt.print.*;
 import java.util.ArrayList;
 
-public class MusicBrainzLabelGenerator {
+public class MusicBrainzLabelGenerator implements Printable {
     private ArrayList<MusicBrainzFinalizedRelease> finalizedReleaseList;
+
+    @Override
+    public int print(java.awt.Graphics graphics, java.awt.print.PageFormat pageFormat, int pageIndex) {
+        if (pageIndex > 0) {
+            return NO_SUCH_PAGE;
+        }
+
+        Graphics2D g2d = (Graphics2D) graphics;
+        g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+        g2d.setFont(new Font("Arial", Font.PLAIN, 12));
+        g2d.setColor(Color.BLACK);
+        g2d.drawString("CD Labels", 0, 0);
+
+        return PAGE_EXISTS;
+    }
 
     public MusicBrainzLabelGenerator() {
         finalizedReleaseList = new ArrayList<>();
@@ -21,5 +38,39 @@ public class MusicBrainzLabelGenerator {
 
     public void addRelease(MusicBrainzFinalizedRelease release) {
         finalizedReleaseList.add(release);
+    }
+
+    public void printLabel() {
+        PrinterJob job = PrinterJob.getPrinterJob();
+        job.setPrintable(this);
+
+        boolean doPrint = job.printDialog();
+        if (doPrint) {
+            try {
+                job.print();
+            } catch (PrinterException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private double[] getDPI() {
+        PrinterJob job = PrinterJob.getPrinterJob();
+        PageFormat pageFormat = job.defaultPage();
+        Paper paper = pageFormat.getPaper();
+
+        // Get the width and height of the paper in points (1 point = 1/72 inch)
+        double widthInPoints = paper.getWidth();
+        double heightInPoints = paper.getHeight();
+
+        // Assume standard paper size (e.g., 8.5 x 11 inches for Letter)
+        double widthInInches = 8.5;
+        double heightInInches = 11.0;
+
+        // Calculate DPI
+        double dpiX = widthInPoints / widthInInches;
+        double dpiY = heightInPoints / heightInInches;
+
+        return new double[]{dpiX, dpiY};
     }
 }
